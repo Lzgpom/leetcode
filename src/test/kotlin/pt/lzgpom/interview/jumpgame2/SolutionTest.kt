6025d1d77e794755b95a6c61f268f7ad827d3625
Lzgpom <1170954@isep.ipp.pt>
2024-04-22 19:20:57 +0100

    completed jump game 2 problem

---
+++ src/test/kotlin/pt/lzgpom/interview/jumpgame2/SolutionTest.kt
@@ -0,0 +1,60 @@
+package pt.lzgpom.interview.jumpgame2
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun testWithExample() {
+        // GIVEN
+        val nums = intArrayOf(2, 3, 1, 1, 4)
+        val expected = 2
+
+        // WHEN
+        val result = solution.jump(nums)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample2() {
+        // GIVEN
+        val nums = intArrayOf(2, 3, 0, 1, 4)
+        val expected = 2
+
+        // WHEN
+        val result = solution.jump(nums)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample3() {
+        // GIVEN
+        val nums = intArrayOf(0)
+        val expected = 0
+
+        // WHEN
+        val result = solution.jump(nums)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample4() {
+        // GIVEN
+        val nums = intArrayOf(1, 2, 3)
+        val expected = 2
+
+        // WHEN
+        val result = solution.jump(nums)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+}
