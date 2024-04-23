ef3e700d96a2592b5d66c920e68bb7c9afd0e7d1
Luis Marques (CTW) <Luis.Marques@ctw.bmwgroup.com>
2024-04-23 19:52:15 +0100

    completed h-index problem

---
+++ src/test/kotlin/pt/lzgpom/interview/hindex/SolutionTest.kt
@@ -0,0 +1,60 @@
+package pt.lzgpom.interview.hindex
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun testWithExample() {
+        // GIVEN
+        val nums1 = intArrayOf(3, 0, 6, 1, 5)
+        val expected = 3
+
+        // WHEN
+        val result = solution.hIndex(nums1)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample2() {
+        // GIVEN
+        val nums1 = intArrayOf(1, 3, 1)
+        val expected = 1
+
+        // WHEN
+        val result = solution.hIndex(nums1)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample3() {
+        // GIVEN
+        val nums1 = intArrayOf(11, 15)
+        val expected = 2
+
+        // WHEN
+        val result = solution.hIndex(nums1)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample4() {
+        // GIVEN
+        val nums1 = intArrayOf(4, 4, 0, 0)
+        val expected = 2
+
+        // WHEN
+        val result = solution.hIndex(nums1)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+}
