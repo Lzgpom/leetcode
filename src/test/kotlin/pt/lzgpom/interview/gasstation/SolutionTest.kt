dc9a15115b49ee6cf0a43d80839e98dac2dc89f9
Luis Marques (CTW) <Luis.Marques@ctw.bmwgroup.com>
2024-04-25 01:08:29 +0100

    completed gas station problem

---
+++ src/test/kotlin/pt/lzgpom/interview/gasstation/SolutionTest.kt
@@ -0,0 +1,36 @@
+package pt.lzgpom.interview.gasstation
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
+
+class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun testWithExample() {
+        // GIVEN
+        val gas = intArrayOf(1, 2, 3, 4, 5)
+        val cost = intArrayOf(3, 4, 5, 1, 2)
+        val expected = 3
+
+        // WHEN
+        val result = solution.canCompleteCircuit(gas, cost)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample2() {
+        // GIVEN
+        val gas = intArrayOf(2, 3, 4)
+        val cost = intArrayOf(3, 4, 3)
+        val expected = -1
+
+        // WHEN
+        val result = solution.canCompleteCircuit(gas, cost)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+}
