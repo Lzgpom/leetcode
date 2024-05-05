a6e3fe60e0d297e0e38a5f7eaa1fb19f411d35c6
Lzgpom <1170954@isep.ipp.pt>
2024-05-05 19:33:03 +0100

    completed roman to integer problem

---
+++ src/test/kotlin/pt/lzgpom/interview/romantointeger/SolutionTest.kt
@@ -0,0 +1,48 @@
+package pt.lzgpom.interview.romantointeger
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+
+    private val solution = Solution()
+
+    @Test
+    fun testWithExample() {
+        // GIVEN
+        val input = "III"
+        val expected = 3
+
+        // WHEN
+        val result = solution.romanToInt(input)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample2() {
+        // GIVEN
+        val input = "LVIII"
+        val expected = 58
+
+        // WHEN
+        val result = solution.romanToInt(input)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample3() {
+        // GIVEN
+        val input = "MCMXCIV"
+        val expected = 1994
+
+        // WHEN
+        val result = solution.romanToInt(input)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+}
