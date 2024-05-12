8a7f51d95bd71fac219972fea26a3f8a7a520666
Lzgpom <1170954@isep.ipp.pt>
2024-05-12 19:41:55 +0100

    completed find the index of the first occurrence in a string problem

---
+++ src/main/kotlin/pt/lzgpom/interview/findtheindexofthfirstoccurrenceinastring/Solution.kt
@@ -0,0 +1,25 @@
+package pt.lzgpom.interview.findtheindexofthfirstoccurrenceinastring
+
+class Solution {
+    fun strStr(haystack: String, needle: String): Int {
+        val lastValidIndex = haystack.length - needle.length
+
+        for (i in 0..lastValidIndex) {
+            if (isSameAsNeedle(haystack, i, needle)) {
+                return i
+            }
+        }
+
+        return -1
+    }
+
+    private fun isSameAsNeedle(haystack: String, start: Int, needle: String): Boolean {
+        for (i in needle.indices) {
+            if (needle[i] != haystack[start + i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
