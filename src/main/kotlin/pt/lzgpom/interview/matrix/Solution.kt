0c665d5c74e424f2418d1ad1bb46b3522788a204
Lzgpom <1170954@isep.ipp.pt>
2024-05-23 21:47:02 +0100

    completed valid sudoku problem

---
+++ src/main/kotlin/pt/lzgpom/interview/matrix/Solution.kt
@@ -0,0 +1,34 @@
+package pt.lzgpom.interview.matrix
+
+class Solution {
+    fun isValidSudoku(board: Array<CharArray>): Boolean {
+        val rows = Array<MutableSet<Char>>(9) { HashSet() }
+        val columns = Array<MutableSet<Char>>(9) { HashSet() }
+        val squares = Array<MutableSet<Char>>(9) { HashSet() }
+
+        for (i in board.indices) {
+            for (j in board[i].indices) {
+                val number = board[i][j]
+
+                if (number == '.') {
+                    continue
+                }
+
+                val row = rows[i]
+                val column = columns[j]
+                val square = squares[(i / 3) + ((j / 3) * 3)]
+                val groups = arrayOf(row, column, square)
+
+                for (group in groups) {
+                    if (group.contains(number)) {
+                        return false
+                    }
+
+                    group.add(number)
+                }
+            }
+        }
+
+        return true
+    }
+}
---
+++ src/test/kotlin/pt/lzgpom/interview/matrix/SolutionTest.kt
@@ -0,0 +1,57 @@
+package pt.lzgpom.interview.matrix
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SolutionTest {
+
+    private val solution = Solution()
+
+    @Test
+    fun testWithExample() {
+        // GIVEN
+        val board = arrayOf(
+            charArrayOf('5','3','.','.','7','.','.','.','.'),
+            charArrayOf('6','.','.','1','9','5','.','.','.'),
+            charArrayOf('.','9','8','.','.','.','.','6','.'),
+            charArrayOf('8','.','.','.','6','.','.','.','3'),
+            charArrayOf('4','.','.','8','.','3','.','.','1'),
+            charArrayOf('7','.','.','.','2','.','.','.','6'),
+            charArrayOf('.','6','.','.','.','.','2','8','.'),
+            charArrayOf('.','.','.','4','1','9','.','.','5'),
+            charArrayOf('.','.','.','.','8','.','.','7','9')
+        )
+
+        val expected = true
+
+        // WHEN
+        val result = solution.isValidSudoku(board)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testWithExample2() {
+        // GIVEN
+        val board = arrayOf(
+            charArrayOf('8','3','.','.','7','.','.','.','.'),
+            charArrayOf('6','.','.','1','9','5','.','.','.'),
+            charArrayOf('.','9','8','.','.','.','.','6','.'),
+            charArrayOf('8','.','.','.','6','.','.','.','3'),
+            charArrayOf('4','.','.','8','.','3','.','.','1'),
+            charArrayOf('7','.','.','.','2','.','.','.','6'),
+            charArrayOf('.','6','.','.','.','.','2','8','.'),
+            charArrayOf('.','.','.','4','1','9','.','.','5'),
+            charArrayOf('.','.','.','.','8','.','.','7','9')
+        )
+
+        val expected = false
+
+        // WHEN
+        val result = solution.isValidSudoku(board)
+
+        // THEN
+        assertEquals(expected, result)
+    }
+}
