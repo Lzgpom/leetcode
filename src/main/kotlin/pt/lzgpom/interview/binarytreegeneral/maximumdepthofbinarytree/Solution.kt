3f8d53d4a5be9ddc01baf8a9bcd7dd3ddc0f545e
Lzgpom <1170954@isep.ipp.pt>
2024-06-16 22:22:39 +0100

    completed maximum depth of binary tree problem

--- src/main/kotlin/pt/lzgpom/interview/binarytreegeneral/maximumdepthofbinarytree/Solution.kt
+++ src/main/kotlin/pt/lzgpom/interview/binarytreegeneral/maximumdepthofbinarytree/Solution.kt
@@ -1,9 +1,18 @@
 package pt.lzgpom.interview.binarytreegeneral.maximumdepthofbinarytree
 
 import pt.lzgpom.interview.binarytreegeneral.TreeNode
+import kotlin.math.max
 
 class Solution {
     fun maxDepth(root: TreeNode?): Int {
+        return maxDepth(root, 0)
+    }
 
+    private fun maxDepth(root: TreeNode?, depth: Int): Int {
+        if (root == null) {
+            return depth
+        }
+
+        return max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1))
     }
 }
