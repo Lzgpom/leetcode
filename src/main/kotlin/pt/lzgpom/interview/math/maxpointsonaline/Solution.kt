eb5e93d5dbf91ce4b53cd046ec34b50a98349e9b
Luis Marques (CTW) <Luis.Marques@ctw.bmwgroup.com>
2024-09-27 00:39:20 +0100

    completed max points in a line problem

--- src/main/kotlin/pt/lzgpom/interview/math/maxpointsonaline/Solution.kt
+++ src/main/kotlin/pt/lzgpom/interview/math/maxpointsonaline/Solution.kt
@@ -1,7 +1,69 @@
 package pt.lzgpom.interview.math.maxpointsonaline
 
+import kotlin.math.max
+
 class Solution {
     fun maxPoints(points: Array<IntArray>): Int {
+        if (points.size == 1)  return 1
 
+        var max = 2
+
+        for (i in points.indices) {
+            for (j in i + 1..points.lastIndex) {
+                val equation = getEquationForPoints(points[i], points[j])
+                var count = 0
+
+                for (point in points) {
+                    if (point in equation) {
+                        count++
+                    }
+                }
+
+                max = max(max, count)
+            }
+        }
+
+        return max
+    }
+
+    private fun getEquationForPoints(point1: IntArray, point2: IntArray): Equation {
+        val (x1, y1) = point1.map { it.toDouble() }
+        val (x2, y2) = point2.map { it.toDouble() }
+
+        if (x1 == x2) return XEquation(x1)
+        if (y1 == y2) return YEquation(y1)
+
+        val m = (y2 - y1) / (x2 - x1)
+        val b = y1 - (m * x1)
+
+        return SlopedEquation(m, b)
+    }
+
+    interface Equation {
+        operator fun contains(point: IntArray): Boolean
+    }
+
+    data class XEquation(val x: Double) : Equation {
+        override fun contains(point: IntArray): Boolean {
+            val (pointX, _) = point
+            return x.toInt() == pointX
+        }
+    }
+
+    data class YEquation(val y: Double) : Equation {
+        override fun contains(point: IntArray): Boolean {
+            val (_, pointY) = point
+            return y.toInt() == pointY
+        }
+    }
+
+    data class SlopedEquation(val m: Double, val b: Double) : Equation {
+        override operator fun contains(point: IntArray): Boolean {
+            val (x, y) = point.map { it.toDouble() }
+            var yPotential: Double = m * x + b
+            yPotential = Math.round(yPotential * 10000.0) / 10000.0
+
+            return y == yPotential
+        }
     }
 }
--- src/test/kotlin/pt/lzgpom/interview/math/maxpointsonaline/SolutionTest.kt
+++ src/test/kotlin/pt/lzgpom/interview/math/maxpointsonaline/SolutionTest.kt
@@ -15,7 +15,20 @@ class SolutionTest {
             intArrayOf(4, 1),
             intArrayOf(2, 3),
             intArrayOf(1, 4)
-        ) to 4
+        ) to 4,
+        arrayOf(intArrayOf(0, 0)) to 1,
+        arrayOf(intArrayOf(0, 1), intArrayOf(0, 0)) to 2,
+        arrayOf(intArrayOf(-6, -1), intArrayOf(3, 1), intArrayOf(12, 3)) to 3,
+        arrayOf(
+            intArrayOf(0, 0),
+            intArrayOf(4, 5),
+            intArrayOf(7, 8),
+            intArrayOf(8, 9),
+            intArrayOf(5, 6),
+            intArrayOf(3, 4),
+            intArrayOf(1, 1),
+        ) to 5,
+        arrayOf(intArrayOf(5151, 5150), intArrayOf(0, 0), intArrayOf(5152, 5151)) to 2
     )
 
     @TestFactory
@@ -29,4 +42,5 @@ class SolutionTest {
                 assertEquals(expected, result)
             }
         }
+
 }
