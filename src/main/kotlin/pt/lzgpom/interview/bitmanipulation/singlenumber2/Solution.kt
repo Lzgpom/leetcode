fc8506b1475e810282ef5505a8a20b2b62d7a8d9
Lzgpom <1170954@isep.ipp.pt>
2024-09-26 01:47:57 +0100

    completed single number 2 problem

---
+++ src/main/kotlin/pt/lzgpom/interview/bitmanipulation/singlenumber2/Solution.kt
@@ -0,0 +1,15 @@
+package pt.lzgpom.interview.bitmanipulation.singlenumber2
+
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        var ones = 0
+        var twos = 0
+
+        for (num in nums) {
+            ones = ones xor (num and twos.inv())
+            twos = twos xor (num and ones.inv())
+        }
+
+        return ones
+    }
+}
--- src/test/kotlin/pt/lzgpom/interview/bitmanipulation/singlenumber2/SolutionTest.kt
+++ src/test/kotlin/pt/lzgpom/interview/bitmanipulation/singlenumber2/SolutionTest.kt
@@ -1,4 +1,4 @@
-package pt.lzgpom.interview.bitmanipulation.singlenumber
+package pt.lzgpom.interview.bitmanipulation.singlenumber2
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DynamicTest.dynamicTest
@@ -7,9 +7,8 @@ import org.junit.jupiter.api.TestFactory
 class SolutionTest {
     private val solution = Solution()
     private val examples = listOf(
-        intArrayOf(2, 2, 1) to 1,
-        intArrayOf(4, 1, 2, 1, 2) to 4,
-        intArrayOf(1) to 1,
+        intArrayOf(2, 2, 2, 3) to 3,
+        intArrayOf(0, 1, 0, 1, 0, 1, 99) to 99
     )
 
     @TestFactory
